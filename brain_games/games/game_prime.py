6c9f6ab5c8d1a303edfee02e73d75e1507e7ea82
immuneQQ <vitalyasatka@gmail.com>
2022-12-13 01:57:37 +0500

    edit is_prime

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -19,13 +19,12 @@ def prime_logic():
 
 
 def is_prime(x):
-    if x == 1:
+    if x <= 3:
+        return x > 1
+    if x % 2 == 0 or x % 3 == 0:
         return False
-    if x == 2 or x == 3:
-        return True
-    if x % 2 == 0 or x < 2:
-        return False
-    for i in range(3, int(x ** 0.5) + 1, 2):
-        if x % i == 0:
+    limit = int(x**0.5)
+    for i in range(5, limit+1, 6):
+        if x % i == 0 or x % (i+2) == 0:
             return False
     return True
